ab33f76972c94a0881235dc36058e66eb73d1ed5
aerahrah <201910416@fit.edu.ph>
2022-11-01 15:37:10 +0800

    Update categoryListAdapter.kt

--- FirePH_BAMM-master/app/src/main/java/com/example/fireph/categoryListAdapter.kt
+++ FirePH_BAMM-master/app/src/main/java/com/example/fireph/categoryListAdapter.kt
@@ -1,61 +1,67 @@
 package com.example.fireph
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.anychart.AnyChart
-import com.anychart.AnyChartView
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.chart.common.dataentry.ValueDataEntry
-import com.anychart.charts.Pie
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
 class categoryListAdapter : AppCompatActivity() {
-
-    private lateinit var dbref : DatabaseReference
-    private lateinit var userRecyclerview : RecyclerView
+    fun append(arr: Array<String>, element: String): Array<String> {
+        val list: MutableList<String> = arr.toMutableList()
+        System.out.println(element)
+        list.add(element)
+        return list.toTypedArray()
+    }
+    private lateinit var  userRecyclerview : RecyclerView
     private lateinit var userArrayList : ArrayList<Categories>
+    private lateinit var totalAmountText : TextView
+    private lateinit var buttonIncome : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
 
         userRecyclerview = findViewById(R.id.categoryList)
         userRecyclerview.layoutManager = LinearLayoutManager(this)
         userRecyclerview.setHasFixedSize(true)
+        totalAmountText = findViewById(R.id.textAmount)
+        buttonIncome = findViewById(R.id.buttonIncome)
+
         userArrayList = arrayListOf<Categories>()
         getUserData()
 
+        buttonIncome.setOnClickListener {
+            val intent = Intent(this, CategoryIncomeAdapter::class.java)
+            startActivity(intent)
+            finish()
+            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left)
+        }
     }
 
     private fun getUserData() {
-
-            val uid = FirebaseAuth.getInstance().currentUser!!.uid
-            FirebaseDatabase.getInstance().getReference("Users").child(uid).child("ExpensesHistory")
+        var totalAmount = 0.0
+        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+        FirebaseDatabase.getInstance().getReference("Users").child(uid).child("ExpensesHistory")
             .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     if (snapshot.exists()){
                         for (userSnapshot in snapshot.children){
                             val user = userSnapshot.getValue(Categories::class.java)
+                            totalAmount += user!!.fireAmount!!.toFloat()
                             userArrayList.add(user!!)
                         }
                         userRecyclerview.adapter = categoryAdapter(userArrayList)
+                        totalAmountText.setText(totalAmount.toString()+"php")
                     }
                 }
                 override fun onCancelled(databaseError: DatabaseError) {
                     throw databaseError.toException()
                 }
             })
-
-//            override fun onCancelled(error: DatabaseError) {
-//                TODO("Not yet implemented")
-//            }
-//
-//
-//        })
-
     }
 }

ac6758441b0b1b50b9c47508762edc13f996161e
aerahrah <201910416@fit.edu.ph>
2022-11-01 15:37:27 +0800

    Create CategoryIncomeAdapter.kt

---
+++ FirePH_BAMM-master/app/src/main/java/com/example/fireph/CategoryIncomeAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.fireph
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+
+class CategoryIncomeAdapter : AppCompatActivity() {
+    private lateinit var  userRecyclerview : RecyclerView
+    private lateinit var userArrayList : ArrayList<Categories>
+    private lateinit var totalAmountText : TextView
+    private lateinit var buttonExpenses : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_income)
+
+        userRecyclerview = findViewById(R.id.categoryList)
+        userRecyclerview.layoutManager = LinearLayoutManager(this)
+        userRecyclerview.setHasFixedSize(true)
+        totalAmountText = findViewById(R.id.textAmount)
+        buttonExpenses = findViewById(R.id.buttonExpenses)
+
+        userArrayList = arrayListOf<Categories>()
+        getUserData()
+
+        buttonExpenses.setOnClickListener {
+            val intent = Intent(this, categoryListAdapter::class.java)
+            startActivity(intent)
+            finish()
+            overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right)
+        }
+    }
+
+    private fun getUserData() {
+        var totalAmount = 0.0
+        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+        FirebaseDatabase.getInstance().getReference("Users").child(uid).child("IncomeHistory")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()){
+                        for (userSnapshot in snapshot.children){
+                            val user = userSnapshot.getValue(Categories::class.java)
+                            totalAmount += user!!.fireAmount!!.toFloat()
+                            userArrayList.add(user!!)
+                        }
+                        userRecyclerview.adapter = categoryAdapter(userArrayList)
+                        totalAmountText.setText(totalAmount.toString()+"php")
+                    }
+                }
+                override fun onCancelled(databaseError: DatabaseError) {
+                    throw databaseError.toException()
+                }
+            })
+    }
+}

47651b6ab9201bf410a1bfd234f99acbda174f08
aerahrah <201910416@fit.edu.ph>
2022-11-03 23:35:02 +0800

    Update monthlyListAdapter.kt

--- FirePH_BAMM-master/app/src/main/java/com/example/fireph/monthlyListAdapter.kt
+++ FirePH_BAMM-master/app/src/main/java/com/example/fireph/monthlyListAdapter.kt
@@ -14,7 +14,6 @@ class MonthlyListAdapter(private val userList : ArrayList<viewDataModel>) : Recy
 
     inner class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
         private lateinit var dbRef: DatabaseReference
-        //        val empId: TextView = itemView.findViewById(R.id.tvempId)
         val fireAmount: TextView = itemView.findViewById(R.id.tvfireAmount)
         val fireCategory: TextView = itemView.findViewById(R.id.tvfireCategory)
         val fireDate: TextView = itemView.findViewById(R.id.tvfireDate)
@@ -28,7 +27,6 @@ class MonthlyListAdapter(private val userList : ArrayList<viewDataModel>) : Recy
         }
 
         fun deleteItemDatabase(currentitem: viewDataModel){
-
             val uid = FirebaseAuth.getInstance().currentUser!!.uid
             dbRef = FirebaseDatabase.getInstance().getReference("Users").child(uid).child("ExpensesHistory")
             dbRef.child(currentitem.empId.toString()).removeValue()
@@ -40,24 +38,19 @@ class MonthlyListAdapter(private val userList : ArrayList<viewDataModel>) : Recy
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder{
 
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.actity_list,
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.activity_list,
             parent,false)
         return MyViewHolder(itemView)
-
     }
 
     override fun onBindViewHolder(holder: MonthlyListAdapter.MyViewHolder, position: Int) {
 
         val currentitem = userList[position]
         holder.bind(userList[position], position)
-//        holder.empId.text = currentitem.empId
         holder.fireAmount.text = currentitem.fireAmount
         holder.fireCategory.text = currentitem.fireCategory
         holder.fireDate.text = currentitem.fireDate
         holder.fireType.text = currentitem.fireType
-////        holder.category.text = currentitem.Category
-////        System.out.println(holder.empId.text)
-//        System.out.println(holder.fireAmount.text)
     }
     override fun getItemCount(): Int {
 

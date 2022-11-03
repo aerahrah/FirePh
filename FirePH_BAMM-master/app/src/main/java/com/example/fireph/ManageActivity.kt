8fe16acdbfa489974bf6ef105dd568a18233f8ed
aerahrah <201910416@fit.edu.ph>
2022-11-03 23:57:06 +0800

    Update ManageActivity.kt

--- FirePH_BAMM-master/app/src/main/java/com/example/fireph/ManageActivity.kt
+++ FirePH_BAMM-master/app/src/main/java/com/example/fireph/ManageActivity.kt
@@ -12,44 +12,50 @@ import com.google.firebase.database.*
 
 class ManageActivity : AppCompatActivity() {
     private lateinit var userArrayList : ArrayList<Categories>
-    private lateinit var totalAmountText : TextView
+    private lateinit var totalAmountText2 : TextView
+    private lateinit var totalAmountText1 : TextView
     private lateinit var buttonExpenses : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage)
 
-        totalAmountText = findViewById(R.id.textAmount)
-        getUserData()
+        totalAmountText2 = findViewById(R.id.textAmount2)
+        totalAmountText1 = findViewById(R.id.textAmount1)
+        getUserDataIncome()
+        getUserDataExpense()
     }
-
-    private fun getUserData() {
-        var totalAmount = 0.0
+    private fun getUserDataIncome(){
+        var totalAmount1 = 0.0
         val uid = FirebaseAuth.getInstance().currentUser!!.uid
-        FirebaseDatabase.getInstance().getReference("Users").child(uid).child("expenseHistory")
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()){
-                        for (userSnapshot in snapshot.children){
-                            val user = userSnapshot.getValue(Categories::class.java)
-                            totalAmount += user!!.fireAmount!!.toFloat()
-                        }
-                        totalAmountText.setText(totalAmount.toString()+"php")
-                    }
-                }
-                override fun onCancelled(databaseError: DatabaseError) {
-                    throw databaseError.toException()
-                }
-            })
         FirebaseDatabase.getInstance().getReference("Users").child(uid).child("IncomeHistory")
             .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     if (snapshot.exists()){
                         for (userSnapshot in snapshot.children){
                             val user = userSnapshot.getValue(Categories::class.java)
-                            totalAmount += user!!.fireAmount!!.toFloat()
+                            totalAmount1 += user!!.fireAmount!!.toFloat()
                         }
-                        totalAmountText.setText(totalAmount.toString()+"php")
+                        totalAmountText1.setText(totalAmount1.toString()+"php")
+                    }
+                }
+                override fun onCancelled(databaseError: DatabaseError) {
+                    throw databaseError.toException()
+                }
+            })
+    }
+    private fun getUserDataExpense() {
+        var totalAmount2 = 0.0
+        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+        FirebaseDatabase.getInstance().getReference("Users").child(uid).child("ExpensesHistory")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()){
+                        for (userSnapshot in snapshot.children){
+                            val user = userSnapshot.getValue(Categories::class.java)
+                            totalAmount2 += user!!.fireAmount!!.toFloat()
+                        }
+                        totalAmountText2.setText(totalAmount2.toString()+"php")
                     }
                 }
                 override fun onCancelled(databaseError: DatabaseError) {

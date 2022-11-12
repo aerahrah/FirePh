88a2b2111e7c8746cfbea5114bb3589495658c67
aerahrah <201910416@fit.edu.ph>
2022-11-12 22:00:01 +0800

    Update ViewTransactions.kt

--- FirePH_BAMM-master/app/src/main/java/com/example/fireph/ViewTransactions.kt
+++ FirePH_BAMM-master/app/src/main/java/com/example/fireph/ViewTransactions.kt
@@ -65,6 +65,8 @@ class ViewTransactions : AppCompatActivity() {
                         }
                         userRecyclerview.adapter = MonthlyListAdapter(userArrayList)
                         totalAmountText.setText(totalAmount.toString()+"php")
+                    }else{
+                        totalAmountText.setText(totalAmount.toString()+"php")
                     }
                 }
                 override fun onCancelled(databaseError: DatabaseError) {
